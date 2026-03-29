dc5830cebc5f75f90e6f6c6069cec309b5ace0f9
Song Inseok <thddlstjrchlrh@gmail.com>
2026-03-29 15:13:43 +0900

    first

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        // 우선순위 큐 선언 (기본은 오름차순)
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+    }
+}
