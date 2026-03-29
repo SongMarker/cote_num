b9e784873f95ec1eaca0a3e1e9543a5d8afff4fb
Song Inseok <thddlstjrchlrh@gmail.com>
2026-03-29 15:19:06 +0900

    PriorityQueue

--- Main.java
+++ Main.java
@@ -4,22 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        
-        // 우선순위 큐 선언 (기본은 오름차순)
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // 절댓값 기준으로 정렬하는 Comparator 추가
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+
+            if (first_abs == second_abs) {
+                return o1 > o2 ? 1 : -1; // 절댓값이 같으면 실제 값이 작은 순
+            }
+            return first_abs - second_abs; // 절댓값이 작은 순
+        });
 
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             if (x == 0) {
-                if (pq.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(pq.poll());
-                }
+                if (pq.isEmpty()) System.out.println(0);
+                else System.out.println(pq.poll());
             } else {
                 pq.add(x);
-
-                
             }
         }
     }
