e97fc16d92a20a72b181b4348cc2a97e8a4cea9f
Song Inseok <thddlstjrchlrh@gmail.com>
2026-03-29 15:20:25 +0900

    StringBuilder ;
    ;
    ;
    ;

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        // 절댓값 기준으로 정렬하는 Comparator 추가
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
             int first_abs = Math.abs(o1);
             int second_abs = Math.abs(o2);
-
-            if (first_abs == second_abs) {
-                return o1 > o2 ? 1 : -1; // 절댓값이 같으면 실제 값이 작은 순
-            }
-            return first_abs - second_abs; // 절댓값이 작은 순
+            if (first_abs == second_abs) return o1 > o2 ? 1 : -1;
+            return first_abs - second_abs;
         });
 
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
+            int x = Integer.parseInt(br.readLine());
             if (x == 0) {
-                if (pq.isEmpty()) System.out.println(0);
-                else System.out.println(pq.poll());
+                if (pq.isEmpty()) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
             } else {
                 pq.add(x);
             }
         }
+        System.out.println(sb.toString());
     }
 }
